f38fad388da19757180e89b8762154cf1c62c7b0
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-12-27 13:27:25 +0300

    data12.js

--- data12.js
+++ data12.js
@@ -1,3 +1,53 @@
 /* eslint-disable no-unused-vars */
 const gData = [
+	{
+		nome: 'Isidoro Ghezzi ⚡️',
+		'satoshi/€': 10 * Math.sqrt(1000),
+		'telegram-id': '@isghe_home'
+	},
+	{
+		nome: 'F_bas',
+		'satoshi/€': 1250,
+		'telegram-id': '@fb8500'
+	},
+	{
+		nome: 'Guido Baroncini Turicchia',
+		'satoshi/€': 1640,
+		'telegram-id': '@guidobartu'
+	},
+	{
+		nome: 'mordom',
+		'satoshi/€': 3850,
+		'telegram-id': '@mordom'
+	},
+	{
+		nome: 'Michele',
+		'satoshi/€': 5402,
+		'telegram-id': '@micheletnt'
+	},
+	{
+		nome: 'Ma͛rtiño 🐟',
+		'satoshi/€': 1234.56,
+		'telegram-id': '@martinoz'
+	},
+	{
+		nome: 'Luigi',
+		'satoshi/€': 5000,
+		'telegram-id': '@Luigi_Ang'
+	},
+	{
+		nome: 'S.... B....',
+		'satoshi/€': 5000,
+		'telegram-id': null
+	},
+	{
+		nome: 'Andrea zyx',
+		'satoshi/€': 6250,
+		'telegram-id': '@Andreazyx'
+	},
+	{
+		nome: 'Pindol',
+		'satoshi/€': 3350,
+		'telegram-id': '@Pindol'
+	}
 ];
