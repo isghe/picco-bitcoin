04186c4c0ec5a5247a0abe8dfb731397866df805
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2020-01-29 10:56:21 +0100

    11749.5

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ let gController = null;
 			self.model = {
 				title: '#picco12',
 				subTitle: '(#picco2020)',
-				minValue: 12540,
+				// minValue: 12540,
+				minValue: 11749.5, // 29 jan 2020
 				satoshiPerBitcoin: 100000000,
 				columnSatoshiPerBitcoinIndex: 2
 			};
