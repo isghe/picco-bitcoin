024027e667a73d9b6cbc2d3f94a4adafd65f6442
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2024-01-03 08:06:20 +0100

    refactor: folder data

---
+++ data/data15.js
@@ -0,0 +1,293 @@
+/* eslint-disable no-unused-vars */
+const gData15 = [
+	{
+		nome: 'Isidoro Ghezzi 🇮🇹⚡️',
+		'satoshi/€': 10 * Math.sqrt(1000),
+		'telegram-id': '@isghe_home',
+	},
+	{
+		nome: 'robertobipunto',
+		'satoshi/€': 919.25,
+		'telegram-id': '@robertobipunto',
+	},
+	{
+		nome: 'Luigi',
+		'satoshi/€': 2857,
+		'telegram-id': '@Luigi_Ang',
+	},
+	{
+		nome: 'Dan',
+		'satoshi/€': 2121,
+		'telegram-id': '@daniel_exploit',
+	},
+	{
+		nome: 'Guido',
+		'satoshi/€': 3703,
+		'telegram-id': '@guidodiso',
+	},
+	{
+		nome: 'Francesco Lupetti',
+		'satoshi/€': 555,
+		'telegram-id': '@Black_and_Gold',
+	},
+	{
+		nome: 'Pindol',
+		'satoshi/€': 2123,
+		'telegram-id': '@Pindol',
+	},
+	{
+		nome: 'F',
+		'satoshi/€': 4275,
+		'telegram-id': '@fra2243',
+	},
+	{
+		nome: 'Valerio Vaccaro [I don\'t ask or send BTC]',
+		'satoshi/€': 300,
+		'telegram-id': '@valerio_vaccaro',
+	},
+	{
+		nome: 'Il_Berti',
+		'satoshi/€': 2778,
+		'telegram-id': '@pippo_berti85',
+	},
+	{
+		nome: 'Andrea zyx',
+		'satoshi/€': 6666,
+		'telegram-id': '@andreazyx',
+	},
+	{
+		nome: 'Cisco Kid',
+		'satoshi/€': 777,
+		'telegram-id': '@Cisco_Kid80',
+	},
+	{
+		nome: 'A',
+		'satoshi/€': 2222,
+		'telegram-id': '@bc_guts',
+	},
+	{
+		nome: 'XX',
+		'satoshi/€': 667,
+		'telegram-id': '@xxxx_telegram',
+	},
+	{
+		nome: 'Spritz03',
+		'satoshi/€': 1987,
+		'telegram-id': '@spritz03',
+	},
+	{
+		nome: 'Davide Coltro',
+		'satoshi/€': 4021,
+		'telegram-id': '@davidecoltro',
+	},
+	{
+		nome: 'Brando',
+		'satoshi/€': 2117,
+		'telegram-id': '@brandosari',
+	},
+	{
+		nome: 'Tom Joad',
+		'satoshi/€': 1305,
+		'telegram-id': '@JoadTom',
+	},
+	{
+		nome: 'TURTLECUTE 丰 | priorato.org ⚡🐢',
+		'satoshi/€': 3700,
+		'telegram-id': '@Turtlecute',
+	},
+	{
+		nome: 'Walter',
+		'satoshi/€': 1741,
+		'telegram-id': '@wallyyy',
+	},
+	{
+		nome: 'Akkarin',
+		'satoshi/€': 2285,
+		'telegram-id': '@hlAkkarin',
+	},
+	{
+		nome: 'Roberto',
+		'satoshi/€': 3333,
+		'telegram-id': '@Fonzarelliii',
+	},
+	{
+		nome: 'Daniela ₿',
+		'satoshi/€': 696,
+		'telegram-id': '@danielabrozzoni',
+	},
+	{
+		nome: 'Manuel G',
+		'satoshi/€': 2121,
+		'telegram-id': '@nondiremanuel',
+	},
+	{
+		nome: 'Salvatore ₿',
+		'satoshi/€': 2424,
+		'telegram-id': '@bitsalv',
+	},
+	{
+		nome: 'digitalkey',
+		'satoshi/€': 2222,
+		'telegram-id': '@digitalkey',
+	},
+	{
+		nome: 'Lorenzo',
+		'satoshi/€': 404,
+		'telegram-id': '@assghhk',
+	},
+	{
+		nome: 'Mark',
+		'satoshi/€': 2941,
+		'telegram-id': '@Rossoscuro',
+	},
+	{
+		nome: 'Peanut Butter',
+		'satoshi/€': 2500,
+		'telegram-id': '@PeanutButterrex',
+	},
+	{
+		nome: 'Giuliano A',
+		'satoshi/€': 684,
+		'telegram-id': '@giulianoassaggio',
+	},
+	{
+		nome: 'Account Eliminato',
+		'satoshi/€': 2350,
+		'telegram-id': '',
+	},
+	{
+		nome: 'Giacomo Zucco',
+		'satoshi/€': 2222,
+		'telegram-id': '@giacomozucco',
+	},
+	{
+		nome: '🤬',
+		'satoshi/€': 1000,
+		'telegram-id': '@MarioRossi_78',
+	},
+	{
+		nome: 'Federico Rivi',
+		'satoshi/€': 3150,
+		'telegram-id': '@federicori',
+	},
+	{
+		nome: 'Swagnacl',
+		'satoshi/€': 3023,
+		'telegram-id': '@swagtoshi',
+	},
+	{
+		nome: 'R. Bancone',
+		'satoshi/€': 3456,
+		'telegram-id': '@Rbancone',
+	},
+	{
+		nome: 'Account Eliminato',
+		'satoshi/€': 2081,
+		'telegram-id': '',
+	},
+	{
+		nome: 'Antonio [CSP]',
+		'satoshi/€': 3333,
+		'telegram-id': '@AC8998',
+	},
+	{
+		nome: 'PEDRO',
+		'satoshi/€': 1405,
+		'telegram-id': '@PEDRO1XXX',
+	},
+	{
+		nome: 'Andrea S. ⚡️🔑',
+		'satoshi/€': 1337,
+		'telegram-id': '@LordStapy',
+	},
+	{
+		nome: 'Django',
+		'satoshi/€': 2121,
+		'telegram-id': '@DJANGO_6',
+	},
+	{
+		nome: 'Stefano',
+		'satoshi/€': 3030,
+		'telegram-id': '@st3w31',
+	},
+	{
+		nome: 'X. P.',
+		'satoshi/€': 1234,
+		'telegram-id': '@xp001',
+	},
+	{
+		nome: 'Raoul',
+		'satoshi/€': 2856,
+		'telegram-id': '@I_I_I_RAOUL_I_I_I',
+	},
+	{
+		nome: 'Andrea NNA 🐍',
+		'satoshi/€': 3461,
+		'telegram-id': '@Andrea_NNA',
+	},
+	{
+		nome: 'Gio396',
+		'satoshi/€': 3096,
+		'telegram-id': '@gio1196',
+	},
+	{
+		nome: 'smartmouse',
+		'satoshi/€': 2500,
+		'telegram-id': '@smartmouse',
+	},
+	{
+		nome: 'Gio396',
+		'satoshi/€': 3096,
+		'telegram-id': '@gio1196',
+	},
+	{
+		nome: 'Zancoborn',
+		'satoshi/€': 69420,
+		'telegram-id': '@SenzaLoStatoLeStradeChiLeFa',
+	},
+	{
+		nome: 'Felicexxx',
+		'satoshi/€': 2660,
+		'telegram-id': '@Felicexxx',
+	},
+	{
+		nome: 'Luca Venturini',
+		'satoshi/€': 399,
+		'telegram-id': '@Lucayepa',
+	},
+	{
+		nome: 'Luca_Despe',
+		'satoshi/€': 1111,
+		'telegram-id': '@Luca_Despe',
+	},
+	{
+		nome: 'Tolo',
+		'satoshi/€': 5543,
+		'telegram-id': '@TanTanTaTaTan',
+	},
+	{
+		nome: 'Fabrizio',
+		'satoshi/€': 2545,
+		'telegram-id': '@osimo_station',
+	},
+	{
+		nome: 'mordom',
+		'satoshi/€': 1042,
+		'telegram-id': '@mordom',
+	},
+	{
+		nome: 'Fausto',
+		'satoshi/€': 2250,
+		'telegram-id': '@VivaJohnGalt',
+	},
+	{
+		nome: '𝓢𝓪𝓽𝓸𝓼𝓱𝓲฿',
+		'satoshi/€': 3000,
+		'telegram-id': '@mooramask',
+	},
+	{
+		nome: 'GIAN',
+		'satoshi/€': 2100,
+		'telegram-id': '@splfrtsz',
+	},
+];
