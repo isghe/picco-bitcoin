11672f437bccaa684e1322112e69ba616719b3b3
cyphersats <82292551+cyphersats@users.noreply.github.com>
2024-01-07 23:00:32 +0100

    Update picco.js

--- picco.js
+++ picco.js
@@ -61,6 +61,7 @@ const gPicco = {
 		title: '#picco16',
 		subTitle: '(#picco2024)',
 		favicon: 'p16.png',
-		minValue: 100000000000, // sort of infinite - before 3 jan 2024
+		// minValue: 100000000000, // sort of infinite - before 3 jan 2024
+		minValue: 2445.420656, // 4 gennaio 2023
 	},
 };
