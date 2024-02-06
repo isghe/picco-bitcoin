1c8699b3ad61031ae1ab0dbc88e23a4328470761
cyphersats <82292551+cyphersats@users.noreply.github.com>
2024-02-06 01:32:20 +0100

    Update minValue

--- picco.js
+++ picco.js
@@ -63,5 +63,6 @@ const gPicco = {
 		favicon: 'p16.png',
 		// minValue: 100000000000, // sort of infinite - before 3 jan 2024
 		minValue: 2445.420656, // 4 gennaio 2023
+		minValue: 2236.811702, // 11 gennaio 2023
 	},
 };
