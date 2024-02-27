c4277d4861bb8ef929300e2614f1d63853bf289f
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2024-02-27 11:46:37 +0100

    minValue: 1910.8401964343723, // 27 febbraio 2024

--- picco.js
+++ picco.js
@@ -63,6 +63,7 @@ const gPicco = {
 		favicon: 'p16.png',
 		// minValue: 100000000000, // sort of infinite - before 3 jan 2024
 		// minValue: 2445.420656, // 4 gennaio 2024
-		minValue: 2236.811702, // 11 gennaio 2024
+		// minValue: 2236.811702, // 11 gennaio 2024
+		minValue: 1910.8401964343723, // 27 febbraio 2024
 	},
 };
